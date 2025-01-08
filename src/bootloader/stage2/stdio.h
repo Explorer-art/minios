6d767d7dcdf5a300135b6c6ebfaba017d47ac22d
Truzme_ <56657421+Explorer-art@users.noreply.github.com>
2025-01-08 12:48:46 +0300

    Delete src directory

--- src/bootloader/stage2/stdio.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void putchar(int ic);
-void puts(const char* str);
-void _cdecl printf(const char* fmt, ...);
--- src/bootloader/stage2/string.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-char* strcpy(char* dst, char* src);
-const char* strchr(const char* str, int symbol);
-unsigned strlen(const char* str);

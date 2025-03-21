f38faa69be2bd4daaee2fc4058519f495a019d2c
Truzme_ <56657421+Explorer-art@users.noreply.github.com>
2025-03-21 22:02:17 +0300

    Added std.inc

--- program.asm
+++ program.asm
@@ -3,27 +3,15 @@ org 0x25400
 
 %define ENDL 0x0A, 0x0D
 
-mov si, msg_hello
-call print_string
-
-ret
-
-print_string:
+start:
+	mov si, msg_test
+    mov ax, 15
     push ax
-    xor ax, ax
-
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    push ax
-    call [5000h]
+	call printf
     pop ax
 
-    jmp .loop
+	ret
 
-.done:
-    pop ax
-    ret
+%include 'lib/std.inc'
 
-msg_hello           db "Hello from userland!", ENDL, 0
+msg_test			db "Value: %d Aboba", ENDL, 0
